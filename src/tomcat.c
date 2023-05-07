26e291bdaa05fa39ae499361d9f4d69795a0780d
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-05-07 12:04:10 +0100

    print errors to stderr

--- src/tomcat.c
+++ src/tomcat.c
@@ -8,14 +8,17 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
         FILE *fp = fopen(argv[i], "r");
         if (fp == NULL) {
-            printf(
-                "%s: %s: No such file or directory\n", PROGRAM_NAME, argv[i]
+            fprintf(
+                stderr,
+                "%s: %s: No such file or directory\n",
+                PROGRAM_NAME,
+                argv[i]
             );
             continue;
         }
         DIR *dp = opendir(argv[i]);
         if (dp != NULL) {
-            printf("%s: %s: Is a directory\n", PROGRAM_NAME, argv[i]);
+            fprintf(stderr, "%s: %s: Is a directory\n", PROGRAM_NAME, argv[i]);
             closedir(dp);
             continue;
         }
